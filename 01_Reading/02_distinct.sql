b00e058a8727299c953d71ae8f92a94b1dd4b228
mariajaramillo7 <mariajara96@gmail.com>
2026-07-25 21:43:50 -0500

    Clarify comments in distinct.sql
    Updated comments to clarify SQL queries for distinct values.

--- 01_Reading/02_distinct.sql
+++ 01_Reading/02_distinct.sql
@@ -3,8 +3,12 @@ DISTINCT
 Lección 9.1: https://youtu.be/OuJerKzV5T0?t=6089
 */
 
--- Obtiene todos los datos distintos entre sí de la tabla "users"
+--- Obtiene valores únicos (de una columna con duplicados)
+SELECT DISTINCT _column1_, _column2, ..._  
+FROM _table_name_;
+
+- Obtiene todos los datos distintos entre sí de la tabla "users"
 SELECT DISTINCT * FROM users;
 
--- Obtiene todos los valores distintos referentes al atributo edad de la tabla "users"
+- Obtiene todos los valores distintos referentes al atributo edad de la tabla "users"
 SELECT DISTINCT age FROM users;
