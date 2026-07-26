fbcfb4ff8835c7f80c5b4d5e1754b752eae4fb8a
mariajaramillo7 <mariajara96@gmail.com>
2026-07-26 09:55:16 -0500

    Revise SQL insert statements and documentation
    Updated comments and examples for inserting records in the 'users' table.

--- 02_Writing/01_insert.sql
+++ 02_Writing/01_insert.sql
@@ -3,11 +3,20 @@ INSERT
 Lección 11.1: https://youtu.be/OuJerKzV5T0?t=10370
 */
 
--- Inserta un registro con identificador, nombre y apellido en la tabla "users"
+--- Insertar nuevos registros en una tabla
+Insertar valores para TODAS las columnas - orden de valores debe ser el mismo que orden de las columnas.
+INSERT INTO table_name
+VALUES (value1, value2, value3, ...);
+
+Insertar valores especificando nombres de columnas:  
+INSERT INTO table_name
+VALUES (value1, value2, value3, ...); 
+
+- Inserta un registro con identificador, nombre y apellido en la tabla "users"
 INSERT INTO users (user_id, name, surname) VALUES (8, 'María', 'López')
 
--- Inserta un registro con nombre y apellido en la tabla "users"
+- Inserta un registro con nombre y apellido en la tabla "users"
 INSERT INTO users (name, surname) VALUES ('Paco', 'Pérez')
 
--- Inserta un registro con identificador no correlativo, nombre y apellido en la tabla "users"
+- Inserta un registro con identificador no correlativo, nombre y apellido en la tabla "users"
 INSERT INTO users (user_id, name, surname) VALUES (11, 'El', 'Merma')
