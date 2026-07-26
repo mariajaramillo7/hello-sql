ccd10235417980779ed9ecb384d82de88e1d4964
mariajaramillo7 <mariajara96@gmail.com>
2026-07-25 21:49:38 -0500

    Update SQL comments for clarity and consistency

--- 01_Reading/04_order_by.sql
+++ 01_Reading/04_order_by.sql
@@ -3,17 +3,22 @@ ORDER BY
 Lección 9.3: https://youtu.be/OuJerKzV5T0?t=6592
 */
 
--- Ordena todos los datos de la tabla "users" por edad (ascendente por defecto)
+--- Ordena (sort) registros en un orden (ascendente por default).  
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;
+
+- Ordena todos los datos de la tabla "users" por edad (ascendente por defecto)
 SELECT * FROM users ORDER BY age;
 
--- Ordena todos los datos de la tabla "users" por edad de manera ascendente
+- Ordena todos los datos de la tabla "users" por edad de manera ascendente
 SELECT * FROM users ORDER BY age ASC;
 
--- Ordena todos los datos de la tabla "users" por edad de manera descendente
+- Ordena todos los datos de la tabla "users" por edad de manera descendente
 SELECT * FROM users ORDER BY age DESC;
 
--- Obtiene todos los datos de la tabla "users" con email igual a sara@gmail.com y los ordena por edad de manera descendente
+- Obtiene todos los datos de la tabla "users" con email igual a sara@gmail.com y los ordena por edad de manera descendente
 SELECT * FROM users WHERE email='sara@gmail.com' ORDER BY age DESC;
 
--- Obtiene todos los nombres de la tabla "users" con email igual a sara@gmail.com y los ordena por edad de manera descendente
+- Obtiene todos los nombres de la tabla "users" con email igual a sara@gmail.com y los ordena por edad de manera descendente
 SELECT name FROM users WHERE email='sara@gmail.com' ORDER BY age DESC;
