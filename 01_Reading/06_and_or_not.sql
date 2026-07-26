7b953e640a6e10d4f3a02885dc449bf0c7097a2a
mariajaramillo7 <mariajara96@gmail.com>
2026-07-25 22:00:31 -0500

    Enhance SQL examples for logical conditions
    Added SQL examples demonstrating the use of AND, OR, and NOT conditions in queries.

--- 01_Reading/06_and_or_not.sql
+++ 01_Reading/06_and_or_not.sql
@@ -3,11 +3,51 @@ NOT, AND, OR
 Lección 9.5: https://youtu.be/OuJerKzV5T0?t=7194
 */
 
--- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com
+--- Filtran registros basados en una o más condiciones.
+AND: muestra en registro si TODAS las condiciones son TRUE 
+  SELECT _column1_, _column2, ..._  
+  FROM _table_name_  
+  WHERE _condition1_ AND _condition2_ AND _condition3 ..._;
+
+- Seleccionar todos los Customers donde Country es "Brazil" y City es "Rio de Janeiro" + CustomerID es mayor de 50:
+SELECT * FROM Customers
+WHERE Country = 'Brazil'
+AND City = 'Rio de Janeiro'
+AND CustomerID > 50;
+  
+OR: muestra un registro si CUALQUIER de las condiciones es TRUE
+  SELECT _column1_, _column2, ..._  
+  FROM _table_name_  
+  WHERE _condition1_ OR _condition2_ OR _condition3 ..._;
+
+- Seleccionar todos los registros donde City es "Berlin", o CustomerName empiece con "G", o Country es "Norway":
+SELECT * FROM Customers
+WHERE City = 'Berlin' OR CustomerName LIKE 'G%' OR Country = 'Norway';
+
+NOT:  invierte una condición → excluye datos que no cumplen con un criterio específico
+  SELECT _column1_, _column2, ..._  
+  FROM _table_name_  
+  WHERE NOT _condition_;
+
+- NOT IN
+- NOT LIKE
+- NOT BETWEEN
+  
+- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com
 SELECT * FROM users WHERE NOT email = 'sara@gmail.com';
 
--- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com y edad igual a 15
+--- Se pueden combinar 
+- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com y edad igual a 15
 SELECT * FROM users WHERE NOT email = 'sara@gmail.com' AND age = 15;
 
--- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com o edad igual a 15
+- Obtiene todos datos de la tabla "users" con email distinto a sara@gmail.com o edad igual a 15
 SELECT * FROM users WHERE NOT email = 'sara@gmail.com' OR age = 15;
+
+- Select all Spanish customers that starts with either "G" or "R":
+SELECT * FROM Customers
+WHERE Country = 'Spain' AND (CustomerName LIKE 'G%' OR CustomerName LIKE 'R%');
+
+-Select customers that does not start with the letter 'A':
+SELECT * FROM Customers
+WHERE CustomerName NOT LIKE 'A%';
+
