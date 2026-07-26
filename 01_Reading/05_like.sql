876159b7393c036d4dda457c9d862f4ba9dd5e83
mariajaramillo7 <mariajara96@gmail.com>
2026-07-25 21:51:27 -0500

    Clarify LIKE usage in SQL comments
    Updated comments to clarify usage of LIKE in SQL queries.

--- 01_Reading/05_like.sql
+++ 01_Reading/05_like.sql
@@ -3,11 +3,16 @@ LIKE
 Lección 9.4: https://youtu.be/OuJerKzV5T0?t=6894
 */
 
--- Obtiene todos datos de la tabla "users" que contienen un email con el texto "gmail.com" en su parte final
+--- Busca un patrón de texto específico en una columna.
+Se usa con WHERE para encontrar coincidencias parciales usando wildcards (comodines).
+ - Signo de porcentaje (%): Representa cero, uno o varios caracteres
+ - Guion bajo (_): Representa un solo carácter exacto
+ 
+- Obtiene todos datos de la tabla "users" que contienen un email con el texto "gmail.com" en su parte final
 SELECT * FROM users WHERE email LIKE '%gmail.com';
 
--- Obtiene todos datos de la tabla "users" que contienen un email con el texto "sara" en su parte inicial
+- Obtiene todos datos de la tabla "users" que contienen un email con el texto "sara" en su parte inicial
 SELECT * FROM users WHERE email LIKE 'sara%';
 
--- Obtiene todos datos de la tabla "users" que contienen un email una arroba
+- Obtiene todos datos de la tabla "users" que contienen un email una arroba
 SELECT * FROM users WHERE email LIKE '%@%';
