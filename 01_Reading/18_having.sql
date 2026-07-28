e7a80e1412dd44206bae05931f8d8111b4d64ee0
mariajaramillo7 <mariajara96@gmail.com>
2026-07-28 17:31:37 -0500

    Add HAVING clause example in SQL
    Added SQL example for using HAVING clause with COUNT.

--- 01_Reading/18_having.sql
+++ 01_Reading/18_having.sql
@@ -3,5 +3,13 @@ HAVING
 Lección 10.12: https://youtu.be/OuJerKzV5T0?t=9265
 */
 
+- Filtra grupos de registros creados por la cláusula GROUP BY, usando funciones de agregación como COUNT, SUM o AVG.
+SELECT column1, aggregate_function(column2), column3, ...
+FROM table_name
+WHERE condition
+GROUP BY column1, column3
+HAVING condition -- The condition on grouped data
+ORDER BY column_name;
+
 -- Cuenta cuantas filas contienen un dato no nulo en el campo edad de la tabla "users" mayor que 3
 SELECT COUNT(age) FROM users HAVING COUNT(age) > 3
