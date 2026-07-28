50550aa403f4843d280fb08c1bc80e0083821e95
mariajaramillo7 <mariajara96@gmail.com>
2026-07-28 17:45:30 -0500

    Enhance SQL case.sql with CASE statement details
    Added explanation of CASE statement usage in SQL with examples.

--- 01_Reading/19_case.sql
+++ 01_Reading/19_case.sql
@@ -3,6 +3,25 @@ CASE
 Lección 10.13: https://youtu.be/OuJerKzV5T0?t=9486
 */
 
+--- Define distintos resultados basado en condiciones específicas → es como un if-then-else statement, revisa las condiciones y para en la primera que sea TRUE.
+Si ninguna es TRUE, regresa el valor en el ELSE clasue. Si no hay ELSE, regresa NULL.
+
+Resultados pueden ser:
+Strings
+Números
+Booleans
+Dates - Times
+Null values
+	
+SELECT *,
+CASE
+  WHEN condition1 THEN result1
+  WHEN condition2 THEN result2
+  WHEN conditionN THEN resultN
+  ELSE default_result
+END AS nombre_alias
+FROM table_name;
+
 -- Obtiene todos los datos de la tabla "users" y establece condiciones de visualización de cadenas de texto según el valor de la edad 
 SELECT *,
 CASE 
