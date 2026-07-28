7b08ce2a20a8b98cd398562a8b487ba4b02b3eb5
mariajaramillo7 <mariajara96@gmail.com>
2026-07-28 16:14:49 -0500

    Update 10_count.sql

--- 01_Reading/10_count.sql
+++ 01_Reading/10_count.sql
@@ -8,3 +8,9 @@ Select COUNT(*) FROM users;
 
 -- Cuenta cuantas filas contienen un dato no nulo en el campo edad de la tabla "users"
 Select COUNT(age) FROM users;
+
+-- Contar # de unique quieres de una columna.
+SELECT COUNT(DISTINCT column_name) FROM database;
+
+
+SELECT **COUNT(*column1*) FROM Customers;
