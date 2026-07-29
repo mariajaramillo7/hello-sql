5c9b55fa06a47167c5623430df6ab883049d5efe
mariajaramillo7 <mariajara96@gmail.com>
2026-07-28 21:34:59 -0500

    Fix typo and add SQL update examples

--- 02_Writing/02_update.sql
+++ 02_Writing/02_update.sql
@@ -1,7 +1,14 @@
 /*
-UPDATE
+UUPDATE
 Lección 11.2: https://youtu.be/OuJerKzV5T0?t=10621
 */
+
+---- Actualiza o modifica uno o más registros.
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+WHERE: especifica cuáles registros deben ser actualizados. OJO: Si la omites, se actualizan TODOS los registros.
 
 -- Estable el valor 21 para la edad del registro de la tabla "users" con identificador igual a 11
 UPDATE users SET age = '21' WHERE user_id = 11
@@ -11,3 +18,8 @@ UPDATE users SET age = '20' WHERE user_id = 11
 
 -- Estable edad y una fecha para registro de la tabla "users" con identificador igual a 11
 UPDATE users SET age = 20, init_date = '2020-10-12' WHERE user_id = 11
+
+-- Actualizar ContactName a 'Juan' para todos los registros donde el país es Mexico
+UPDATE Customers
+SET ContactName='Juan'
+WHERE Country='Mexico';
