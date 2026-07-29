d3548cbd02665213b6eb4b1625187d0f7eacdb26
mariajaramillo7 <mariajara96@gmail.com>
2026-07-28 21:40:54 -0500

    Enhance delete.sql with SQL delete examples
    Added comments and examples for SQL delete operations.

--- 02_Writing/03_delete.sql
+++ 02_Writing/03_delete.sql
@@ -3,5 +3,17 @@ DELETE
 Lección 11.3: https://youtu.be/OuJerKzV5T0?t=10920
 */
 
--- Elimina el registro de la tabla "users" con identificador igual a 11
+--- Elimina registros de una tabla. 
+DELETE FROM table_name
+WHERE condition;
+
+WHERE: especifica qué registros eliminar. OJO: si la omites, se eliminan todos los registros. 
+
+- Elimina el registro de la tabla "users" con identificador igual a 11
 DELETE FROM users WHERE user_id = 11;
+
+--- Eliminar todos los registros: borra todo, sin borrar la tabla - mantiene estructura, atributos e indexes. 
+DELETE FROM table_name;
+
+--- Elimina la tabla por completo. 
+DROP TABLE table_name;
