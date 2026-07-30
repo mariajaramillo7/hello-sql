86c6d0b65a104cecca055d870a7ddab9d359744d
mariajaramillo7 <mariajara96@gmail.com>
2026-07-30 18:30:07 -0500

    Update 02_update.sql

--- 02_Writing/02_update.sql
+++ 02_Writing/02_update.sql
@@ -8,13 +8,13 @@ UPDATE table_name
 SET column1 = value1, column2 = value2, ...
 WHERE condition;
 
-WHERE: especifica cuáles registros deben ser actualizados. OJO: Si la omites, se actualizan TODOS los registros.
+WHERE: especifica cuáles registros deben ser actualizados. OJO: Si la omites, se actualizan TODOS los registros. SIEMPRE usar WHERE, es muy raro que tengas que actualizar TODOS.
 
 -- Estable el valor 21 para la edad del registro de la tabla "users" con identificador igual a 11
-UPDATE users SET age = '21' WHERE user_id = 11
+UPDATE users SET age = 21 WHERE user_id = 11
 
 -- Estable el valor 20 para la edad del registro de la tabla "users" con identificador igual a 11
-UPDATE users SET age = '20' WHERE user_id = 11
+UPDATE users SET age = 20 WHERE user_id = 11
 
 -- Estable edad y una fecha para registro de la tabla "users" con identificador igual a 11
 UPDATE users SET age = 20, init_date = '2020-10-12' WHERE user_id = 11
