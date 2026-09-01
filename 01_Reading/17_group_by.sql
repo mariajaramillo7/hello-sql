76fdf90e9dddb41ac447205405bb977622a19ebb
mariajaramillo7 <mariajara96@gmail.com>
2026-09-01 18:54:32 -0500

    Update 17_group_by.sql

--- 01_Reading/17_group_by.sql
+++ 01_Reading/17_group_by.sql
@@ -5,7 +5,12 @@ Lección 10.11: https://youtu.be/OuJerKzV5T0?t=8960
 
 ---Agrupa filas que tienen los mismos valores en filas de resumen 
 Casi siempre se usa junto con aggregate functions como COUNT(), MAX(), MIN(), SUM(), AVG(), to perform calculations on each group.
+
 SELECT column1, aggregate_function(column2), column3, ...
+FROM table_name
+WHERE condition
+GROUP BY column1, column3
+ORDER BY column_name;
 
 -- Agrupa los resultados por edad diferente
 SELECT MAX(age) FROM users GROUP BY age
