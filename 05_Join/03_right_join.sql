3f661845c5bb1db316813286273e2f58f1c0e305
mariajaramillo7 <mariajara96@gmail.com>
2026-09-01 22:50:36 -0500

    Update comment on RIGHT JOIN usage in SQL
    Clarified SQL comment regarding RIGHT JOIN behavior.

--- 05_Join/03_right_join.sql
+++ 05_Join/03_right_join.sql
@@ -5,6 +5,7 @@ Lección 17.3: https://youtu.be/OuJerKzV5T0?t=17399
 
 Obtiene todo lo de la tabla de la derecha + datos comunes. 
 Statements funcionan igual que con INNER JOIN. 
+En algunas DB sale como RIGHT OUTER JOIN.
   
 -- Obtiene todos los dni junto a su usuario (lo tenga o no)
 SELECT * FROM users
