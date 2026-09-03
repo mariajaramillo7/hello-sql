4b0e231c9e52670965c4f80d27c6c6fe4a025e83
mariajaramillo7 <mariajara96@gmail.com>
2026-09-03 16:32:22 -0500

    Enhance transaction.sql with explanatory comments
    Added comments explaining transaction behavior and usage.

--- 06_Advanced/05_transactions.sql
+++ 06_Advanced/05_transactions.sql
@@ -3,6 +3,10 @@ TRANSACTIONS
 Lección 18.5: https://youtu.be/OuJerKzV5T0?t=20501
 */
 
+Algo que se ejecuta en bloque y asegura que eso solo se ejecute si está bien hecha. 
+  Puedo indicarle iniciar transacción, lanzarla, luego revisar si lo que hice está bien. (por ej actualicé un campo que no quiero actualizar, o que no quiero que permita números negativos).
+  Si correcto - commit / si no, me puedo devolver con rollback.
+  
 -- Inicia una nueva transacción. Desde este punto, todas las modificaciones realizadas en la
 -- base de datos son temporales y solo son visibles dentro de esta transacción
 START TRANSACTION
