a7818b28aa58e0b6b53fc235cc7e988b319c3506
mariajaramillo7 <mariajara96@gmail.com>
2026-09-03 18:35:31 -0500

    Update 01_select.sql

--- 01_Reading/01_select.sql
+++ 01_Reading/01_select.sql
@@ -3,11 +3,22 @@ SELECT
 Lección 8: https://youtu.be/OuJerKzV5T0?t=5618
 */
 
---- Extrae/obtiene datos de una base de datos 
+--- Extrae/obtiene datos de una base de datos. Consulta datos de una o varias tablas en una base de datos relacionar.
 SELECT _column1_, _column2, ..._  
 FROM _table_name_;
 
-- Obtiene todos los datos de la tabla "users"
+SELECT *  # Usa * para recuperar todas las columnas
+FROM table_name;
+
+Con otras cláusulas: 
+SELECT column_name
+FROM table_name
+WHERE condition
+DISTINCT 
+ORDER BY ASC/DESC
+LIMIT number;
+  
+  - Obtiene todos los datos de la tabla "users"
 SELECT * FROM users;
 
 - Obtiene todos los nombres de la tabla "users"
