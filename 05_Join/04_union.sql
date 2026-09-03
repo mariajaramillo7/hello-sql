a9172747a3f8d486dfc620c98dae4e4a1dacfcc5
mariajaramillo7 <mariajara96@gmail.com>
2026-09-03 18:47:51 -0500

    Enhance documentation on UNION usage in SQL
    Added explanation of UNION and UNION ALL in MySQL.

--- 05_Join/04_union.sql
+++ 05_Join/04_union.sql
@@ -5,6 +5,14 @@ Lección 17.4: https://youtu.be/OuJerKzV5T0?t=17536
 
 Obtiene todo lo de todas las tablas que estás uniendo, haya o no relaciones. 
 El problema es que en MySQL en concreto no existe este concepto de FULL JOIN - existe comando UNION (unir todo lo de una tabla con todo lo de otra tabla, PERO tengo que decirle con qué relacionarlo)
+El operador UNION permite combinar verticalmente los resultados de dos o más
+consultas SELECT. Las columnas de las consultas a unir deben ser del mismo
+número y tipo, ya que el UNION apila las filas de resultados. Por defecto, UNION
+elimina duplicados entre los resultados combinados; si se desea conservar todas
+las filas (incluyendo duplicados), se puede usar UNION ALL.
+
+
+  
 En algunas DB sale como  FULL OUTER JOIN
   
 -- UNION elimina duplicados
