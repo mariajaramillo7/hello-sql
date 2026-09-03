dc9bf68f5dcf2d96564c0bd8267dda775ec7957e
mariajaramillo7 <mariajara96@gmail.com>
2026-09-03 18:40:33 -0500

    Add comments for DDL and database creation
    Added comments explaining the purpose of DDL statements and the database creation process.

--- 03_Database/01_create_database.sql
+++ 03_Database/01_create_database.sql
@@ -1,7 +1,11 @@
+# Sentencias DDL (Data Definition Language): se usan para crear y administrar bases de datos y sus objetos e.g. tablas. 
+  
 /*
 CREATE DATABASE
 Lección 12.1: https://youtu.be/OuJerKzV5T0?t=11064
 */
 
+Crea una nueva base de datos. 
+  
 -- Crea una base de datos llamada "test"
 CREATE DATABASE test;
