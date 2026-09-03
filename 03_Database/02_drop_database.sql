ec5c938f51fd469742d6b70cf5799f778d655e5e
mariajaramillo7 <mariajara96@gmail.com>
2026-09-03 18:41:32 -0500

    Add caution comment to drop_database.sql
    Added a cautionary comment about database deletion.

--- 03_Database/02_drop_database.sql
+++ 03_Database/02_drop_database.sql
@@ -3,5 +3,7 @@ DROP DATABASE
 Lección 12.2: https://youtu.be/OuJerKzV5T0?t=11180
 */
 
+Elimina una DB (y todas sus tablas). Usar con precaución.
+  
 -- Elimina la base de datos llamada "test"
 DROP DATABASE test;
