9fa6d6140b7ff15bddf1fb49890afb8c512ba3a7
mariajaramillo7 <mariajara96@gmail.com>
2026-09-03 16:20:04 -0500

    Create view for adult users with name and age
    Added comments explaining the purpose of the view and its usage.

--- 06_Advanced/03_views.sql
+++ 06_Advanced/03_views.sql
@@ -3,7 +3,11 @@ VIEWS
 Lección 18.3: https://youtu.be/OuJerKzV5T0?t=19663
 */
 
--- Crea unaa vista llamada "v_adult_users" con los nombres y edades de usuarios de la table "users"
+Representación visual de una o más tablas - resultado de una consulta en formato tabla. 
+  Para simplificar, revisar datos de forma rápida. Crear tabla intermedia que me muestra datos (pero no se crea la tabla como tal)
+  También no es para todos los casos, sí eg para cosas que consulto habitualmente.
+  
+-- Crea una vista llamada "v_adult_users" con los nombres y edades de usuarios de la table "users"
 -- que tienen una edad igual o mayor a 18 años.
 CREATE VIEW v_adult_users AS
 SELECT name, age
