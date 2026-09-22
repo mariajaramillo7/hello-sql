8656293995505ad9b797ade3fcc081240266ff0b
mariajaramillo7 <mariajara96@gmail.com>
2026-09-21 22:37:14 -0500

    Clarify database deletion is permanent
    Updated comment to clarify that the database deletion is permanent.

--- 03_Database/02_drop_database.sql
+++ 03_Database/02_drop_database.sql
@@ -3,7 +3,9 @@ DROP DATABASE
 Lección 12.2: https://youtu.be/OuJerKzV5T0?t=11180
 */
 
-Elimina una DB (y todas sus tablas). Usar con precaución.
-  
+Elimina una DB (y todas sus tablas) de forma permanente. Usar con precaución.
+
+   DROP DATABASE databasename; 
+
 -- Elimina la base de datos llamada "test"
 DROP DATABASE test;
