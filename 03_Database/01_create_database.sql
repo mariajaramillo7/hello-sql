28a9146c695d73978b2901b6573468c18bffcf6d
mariajaramillo7 <mariajara96@gmail.com>
2026-09-21 22:36:34 -0500

    Enhance database creation SQL script
    Added SQL syntax for creating a new database and showing databases.

--- 03_Database/01_create_database.sql
+++ 03_Database/01_create_database.sql
@@ -6,10 +6,18 @@ Lección 12.1: https://youtu.be/OuJerKzV5T0?t=11064
 */
 
 Crea una nueva base de datos. 
-  
+   CREATE DATABASE database_name; 
+
 -- Crea una base de datos llamada "test"
 CREATE DATABASE test;
 
 
 # USE
 Selecciona una DB para que las siguientes operaciones se ejecuten en ella.
+
+Mostrar databases. 
+Syntax for SQL Server
+SELECT name FROM sys.databases;
+
+Syntax for MySQL
+SHOW DATABASES;
