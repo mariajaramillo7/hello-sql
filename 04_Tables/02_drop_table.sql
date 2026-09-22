55e2d004ee0bff216d3b5388fade28e803c1f22b
mariajaramillo7 <mariajara96@gmail.com>
2026-09-21 22:40:02 -0500

    Update drop_table.sql with additional notes
    Added notes on dropping tables and handling foreign key constraints.

--- 04_Tables/02_drop_table.sql
+++ 04_Tables/02_drop_table.sql
@@ -4,6 +4,13 @@ Lección 13.8: https://youtu.be/OuJerKzV5T0?t=12412
 */
 
 Elimina una tabla completa y todos sus datos.
+
+DROP TABLE table_name; 
+
+To prevent an error from occur (if the table does not exists), it is a good practice to add the IF EXISTS clause:
+DROP TABLE IF EXISTS table_name;
+
+Note: In most databases you cannot drop a table that is referenced by a foreign key constraint in another table. To solve this, you must remove the foreign key constraint or drop the dependent table.
   
 -- Elimina la tabla llamada "persons8"
 DROP TABLE persons8;
