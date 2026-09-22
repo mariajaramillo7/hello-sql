81b98bd33a03282f40ab63a2c971b2385566bf74
mariajaramillo7 <mariajara96@gmail.com>
2026-09-21 22:22:09 -0500

    Enhance SQL subquery documentation with ANY and ALL
    Added explanations and examples for ANY and ALL operators in SQL subqueries.

--- 01_Reading/20_subqueries.sql
+++ 01_Reading/20_subqueries.sql
@@ -14,3 +14,48 @@ WHERE EXISTS (
   FROM Products
   WHERE Products.SupplierID = Suppliers.supplierID AND Price < 10
 ); 
+
+
+
+----
+
+The ANY operator is used to compare a value to every value returned by a subquery.
+The ANY operator evaluates to TRUE if at least one value in the subquery result-set meet the condition.
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name operator ANY (subquery); 
+
+The operator must be a standard comparison operator (=, <>, !=, >, >=, <, or <=).
+  
+---The following SQL returns the ProductName if it finds ANY records in the "OrderDetails" table that has Quantity equal to 10 (this will return TRUE because the Quantity column has some values of 10):
+  SELECT ProductName FROM Products
+WHERE ProductID = ANY (
+  SELECT ProductID
+  FROM OrderDetails
+  WHERE Quantity = 10
+); 
+
+-----
+The ALL operator is used to compare a value to every value returned by a subquery.
+
+The ALL operator evaluates to TRUE if every value in the subquery result-set meet the condition.
+
+The ALL operator is typically used with WHERE and HAVING statements.
+
+ SELECT column_name(s)
+FROM table_name
+WHERE column_name operator ALL (subquery); 
+
+The operator must be a standard comparison operator (=, <>, !=, >, >=, <, or <=).
+
+
+---The following SQL returns the ProductName if ALL the records in the "OrderDetails" table has Quantity equal to 10. This will of course return FALSE because the Quantity column has many different values (not only the value of 10):
+SELECT ProductName
+FROM Products
+WHERE ProductID = ALL (
+  SELECT ProductID
+  FROM OrderDetails
+  WHERE Quantity = 10
+); 
+
