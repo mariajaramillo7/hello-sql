5681d3a1a818bb3493b801296692e872fef10600
mariajaramillo7 <mariajara96@gmail.com>
2026-09-21 22:33:54 -0500

    Enhance comments section with explanations
    Added explanations for single-line and multi-line comments.

--- 01_Reading/00_comments.sql
+++ 01_Reading/00_comments.sql
@@ -3,7 +3,12 @@ COMENTAROS
 Lección 10.1: https://youtu.be/OuJerKzV5T0?t=7512
 */
 
--- Comentario en una lína
+Usados para explicar el código o para prevenir temporalmente la ejecución del código (Debugging)
+Los comentarios son ignorados. 
+
+
+-- Comentario en una línEa
+/* */ Comentario en varias líneas
 
 /*
 Este
